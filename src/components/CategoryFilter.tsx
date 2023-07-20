a783bab81f12b8038da2a5d7253aea540dc49c29
Sofiane Pean <pean.sofiane@gmail.com>
2023-07-20 14:57:25 +0200

    Feat: adding styling on hover image

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -8,11 +8,18 @@ interface CategoryFilterProps {
 export const CategoryFilter: React.FC<CategoryFilterProps> = (props) => {
   return (
     <div className="w-full text-center">
+      <span
+        id="badge-dismiss-default"
+        className="inline-flex items-center rounded-full mb-2 px-3 py-2 mr-2 text-xs cursor-pointer lg:text-sm font-medium text-black bg-white rounded"
+        onClick={() => props.getPhotosByCategory("all")}
+      >
+        Toutes
+      </span>
       {props.categories?.map((category) => (
         <span
           key={category.id}
           id="badge-dismiss-default"
-          className="inline-flex items-center rounded-full mb-2 px-2 py-1 mr-2 text-xs lg:text-sm font-medium text-black bg-white rounded"
+          className="inline-flex items-center rounded-full mb-2 px-3 py-2 mr-2 text-xs lg:text-sm cursor-pointer font-medium text-black bg-white rounded"
           onClick={() => props.getPhotosByCategory(category.id)}
         >
           {category.name}
--- src/components/GridGallery.tsx
+++ src/components/GridGallery.tsx
@@ -1,21 +1,23 @@
 "use client";
-
 import { Category } from "@/app/interfaces/category";
 import { Photo } from "@/app/interfaces/photo";
 import Image from "next/image";
 import { useState } from "react";
 import { CategoryFilter } from "./CategoryFilter";
+import { Icon } from "./Icon";
 
 interface GridGalleryProps {
   photos: Photo[];
   categories: Category[];
 }
 
+const ALL = "all";
+
 const GridGallery: React.FC<GridGalleryProps> = (props) => {
   const [photos, setPhotos] = useState<Photo[]>(props.photos);
 
   const getPhotosByCategory = (id: string) => {
-    if (id === "all") {
+    if (id === ALL) {
       setPhotos(props.photos);
     } else {
       const photosFiltered = props.photos.filter(
@@ -35,14 +37,22 @@ const GridGallery: React.FC<GridGalleryProps> = (props) => {
       </div>
       <div className="columns-1 md:columns-3 lg:columns-3 max-w-7xl mt-4 px-4">
         {photos.map((photo) => (
-          <Image
-            key={photo.id}
-            width={500}
-            height={500}
-            alt={photo.description}
-            className="mb-4"
-            src={`${process.env.NEXT_PUBLIC_URL_PHOTO}${photo.url}`}
-          />
+          <div key={photo.id} className="relative overflow-hidden mb-4">
+            <div className="relative group">
+              <Image
+                src={`${process.env.NEXT_PUBLIC_URL_PHOTO}${photo.url}`}
+                alt={photo.description}
+                width={500}
+                height={500}
+                className="transform transition-transform duration-700 cursor-pointer group-hover:scale-125"
+              />
+
+              <div className="absolute top-2 right-2 p-2 opacity-0 transition-opacity duration-700 z-10 ease-in-out group-hover:opacity-100">
+                <Icon icon="full-screen" color="#FFFFFF" size="3em" />
+              </div>
+              <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full opacity-0 transition-opacity duration-700 ease-in-out bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 group-hover:opacity-50"></div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
