703d1eb14a3c24e7a81e0de66f20e563e94c6d6f
Sofiane Pean <pean.sofiane@gmail.com>
2023-07-20 16:18:21 +0200

    Feat: Added the possibility to enlarge images

--- src/components/GridGallery.tsx
+++ src/components/GridGallery.tsx
@@ -15,6 +15,7 @@ const ALL = "all";
 
 const GridGallery: React.FC<GridGalleryProps> = (props) => {
   const [photos, setPhotos] = useState<Photo[]>(props.photos);
+  const [selectedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
 
   const getPhotosByCategory = (id: string) => {
     if (id === ALL) {
@@ -27,8 +28,16 @@ const GridGallery: React.FC<GridGalleryProps> = (props) => {
     }
   };
 
+  const handleImageClick = (photo: Photo) => {
+    setSelectedPhoto(photo);
+  };
+
+  const handleCloseOverlay = () => {
+    setSelectedPhoto(null);
+  };
+
   return (
-    <div className="mt-[-140px] z-10">
+    <div className={`mt-[-140px] ${selectedPhoto ? "z-30" : "z-10"}`}>
       <div className="flex w-full justify-center">
         <CategoryFilter
           categories={props.categories}
@@ -47,7 +56,10 @@ const GridGallery: React.FC<GridGalleryProps> = (props) => {
                 className="transform transition-transform duration-700 cursor-pointer group-hover:scale-125"
               />
 
-              <div className="absolute top-2 right-2 p-2 opacity-0 transition-opacity duration-700 z-10 ease-in-out group-hover:opacity-100">
+              <div
+                className="absolute top-2 right-2 p-2 opacity-0 cursor-pointer transition-opacity duration-700 z-10 ease-in-out group-hover:opacity-100"
+                onClick={() => handleImageClick(photo)}
+              >
                 <Icon icon="full-screen" color="#FFFFFF" size="3em" />
               </div>
               <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full opacity-0 transition-opacity duration-700 ease-in-out bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 group-hover:opacity-50"></div>
@@ -55,6 +67,37 @@ const GridGallery: React.FC<GridGalleryProps> = (props) => {
           </div>
         ))}
       </div>
+      {selectedPhoto && (
+        <div
+          className="relative z-100"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+          onClick={handleCloseOverlay}
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 z-25 transition-opacity"></div>
+
+          <div className="fixed inset-0 z-30 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <div className="relative">
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_URL_PHOTO}${selectedPhoto.url}`}
+                  alt={selectedPhoto.description}
+                  width={600}
+                  height={600}
+                  className="object-contain"
+                />
+              </div>
+              <div
+                className="absolute cursor-pointer font-bold text-xl top-4 right-4"
+                onClick={handleCloseOverlay}
+              >
+                <Icon icon="x" color="#FFFFFF" size="2.5em" />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
